975a18790f75054e93340f0e3ccd64c09932fd4b
Lawjay Lee <lawjaylee1@icloud.com>
2025-03-22 19:05:08 -0700

    Eslint fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center p-24 gap-2 justify-center h-full">
-      <h1 className="text-4xl">Hi! I'm Lawjay!</h1>
-      <p className="text-lg">I'm a computer science student at the CSU Chico!</p>
+    <main className="flex flex-col gap-2 justify-center items-center p-24 h-full">
+      <h1 className="text-4xl">Hi! I&apos;m Lawjay!</h1>
+      <p className="text-lg">
+        I&apos;m a computer science student at the CSU Chico!
+      </p>
     </main>
   );
 }
